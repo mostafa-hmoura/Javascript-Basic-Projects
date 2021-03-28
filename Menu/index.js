cedf4fa84fb1013efcb9207ff568101d745838d9
Mostafa HMOURA <mostafahmoura@gmail.com>
2021-03-28 21:03:25 +0100

    v5

--- Menu/index.js
+++ Menu/index.js
@@ -4,7 +4,7 @@ const menu = [
     title: "SEA-SALT GRAVLAX",
     category: "RUGBY & BULWER",
     price: 10.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -12,7 +12,7 @@ const menu = [
     title: "SALT & SUNFLOWER DRIPPINGS",
     category: "RUGBY & BULWER",
     price: 13.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -20,7 +20,7 @@ const menu = [
     title: "CORN PANINI",
     category: "RUGBY & BULWER",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -28,7 +28,7 @@ const menu = [
     title: "BISON",
     category: "RUGBY & BULWER",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -36,15 +36,15 @@ const menu = [
     title: "WHOLE HAM",
     category: "RUGBY & BULWER",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
     id: 6,
     title: "WHOLE HAM",
-    category: "LIME WAFFLE & WATER",
+    category: "RUGBY & BULWER",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -52,23 +52,23 @@ const menu = [
     title: "WHOLE HAM",
     category: "SUMMER ARUGULA",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
     id: 8,
     title: "WHOLE HAM",
-    category: "FRITTATA WITH BLUEFISH",
+    category: "TORMENTED SALT",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
     id: 9,
     title: "WHOLE HAM",
-    category: "SEAWEED BUN",
+    category: "TORMENTED SALT",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -76,7 +76,7 @@ const menu = [
     title: "WHOLE HAM",
     category: "TORMENTED SALT",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -84,7 +84,7 @@ const menu = [
     title: "distressed corn",
     category: "TORMENTED SALT",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -92,7 +92,7 @@ const menu = [
     title: "gravlax",
     category: "TORMENTED SALT",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -100,7 +100,7 @@ const menu = [
     title: "vinegar with clam",
     category: "TORMENTED SALT",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -108,7 +108,7 @@ const menu = [
     title: "rounded bluefish",
     category: "COOPER & UNION",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
     {
@@ -116,7 +116,72 @@ const menu = [
     title: "pressed monkfish",
     category: "COOPER & UNION",
     price: 14.99,
-    img: "https://picsum.photos/400",
+    img: "img.jpg",
     desc: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Odio ipsam eaque delectus pariatur, sapiente nostrum!"
     },
 ];
+
+
+const section = document.querySelector('.section-center');
+const btn = document.querySelectorAll('button');
+
+window.onload = () => {
+
+    addItem(menu);
+
+    btn.forEach( bt => {
+        bt.addEventListener('click' , e => {
+            addItemFilter(e.currentTarget.innerText,menu);
+        });
+    });
+
+}
+
+const addItem = (tabItem) => {
+    tabItem.map( param => {
+        section.innerHTML += `
+        <article class="menu-item">
+            <img src="https://picsum.photos/500" alt="img" class="photo">
+            <div class="item-info">
+                <header>
+                    <h4>${param.title}</h4>
+                    <h4 class="price">${param.price}$</h4>
+                </header>
+                <p class="item-text">
+                    ${param.desc}
+                </p>
+            </div>
+        </article>
+        `;
+    });
+}
+
+// function for filtring
+
+const addItemFilter = (curentBtnText,tab) => {
+
+    let itemHtml = '';
+    tab.map( param => {
+        if(curentBtnText == 'All') return addItem(menu);
+        if(param.category == curentBtnText){
+            itemHtml += `
+            <article class="menu-item">
+            <img src="https://picsum.photos/500" alt="img" class="photo">
+            <div class="item-info">
+                <header>
+                    <h4>${param.title}</h4>
+                    <h4 class="price">${param.price}$</h4>
+                </header>
+                <p class="item-text">
+                    ${param.desc}
+                </p>
+            </div>
+        </article>
+        `;
+        return section.innerHTML = itemHtml;
+        }
+
+
+    })
+
+}
